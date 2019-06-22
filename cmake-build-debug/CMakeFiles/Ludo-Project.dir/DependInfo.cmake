cdcebbd767edd99cbbeff384002152b5c2247bba
marlonmejia <40769995+marlonmejia@users.noreply.github.com>
2019-06-22 10:11:58 -0500

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/Ludo-Project.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "E:/2019-I/Ludo-Project/Casillas.cpp" "E:/2019-I/Ludo-Project/cmake-build-debug/CMakeFiles/Ludo-Project.dir/Casillas.cpp.obj"
+  "E:/2019-I/Ludo-Project/Dado.cpp" "E:/2019-I/Ludo-Project/cmake-build-debug/CMakeFiles/Ludo-Project.dir/Dado.cpp.obj"
+  "E:/2019-I/Ludo-Project/Fichas.cpp" "E:/2019-I/Ludo-Project/cmake-build-debug/CMakeFiles/Ludo-Project.dir/Fichas.cpp.obj"
+  "E:/2019-I/Ludo-Project/Game.cpp" "E:/2019-I/Ludo-Project/cmake-build-debug/CMakeFiles/Ludo-Project.dir/Game.cpp.obj"
+  "E:/2019-I/Ludo-Project/Player.cpp" "E:/2019-I/Ludo-Project/cmake-build-debug/CMakeFiles/Ludo-Project.dir/Player.cpp.obj"
+  "E:/2019-I/Ludo-Project/Tablero.cpp" "E:/2019-I/Ludo-Project/cmake-build-debug/CMakeFiles/Ludo-Project.dir/Tablero.cpp.obj"
+  "E:/2019-I/Ludo-Project/main.cpp" "E:/2019-I/Ludo-Project/cmake-build-debug/CMakeFiles/Ludo-Project.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../SFML_INCLUDE_DIR"
+  "../SFML-2.5.1/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/Ivan/POO2
+# Install script for directory: E:/2019-I/Ludo-Project
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/POO2")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Ludo-Project")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Ivan/POO2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "E:/2019-I/Ludo-Project/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
