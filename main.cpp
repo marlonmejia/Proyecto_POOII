2dd918631713b3b3e3c04839a49359f0311a09c9
Cronos26 <33726573+Cronos26@users.noreply.github.com>
2019-07-06 12:59:59 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,49 @@
 #include <SFML/Graphics.hpp>
 #include "Game.h"
+#include "Menu.h"
 int main() {
 
-    Game game;
-    game.Run(4);
+    sf::RenderWindow menu(sf::VideoMode(600,600),"Menu De Jugadores");
+    Menu ventanaI(menu.getSize().x,menu.getSize().y);
+    while(menu.isOpen()){
+        sf::Event event;
+        while(menu.pollEvent(event)){
+            switch (event.type){
+                case sf::Event::KeyReleased:
+                    switch (event.key.code){
+                        case sf::Keyboard::Up:
+                            ventanaI.MoveUp();
+                            break;
+                        case sf::Keyboard::Down:
+                            ventanaI.MoveDown();
+                            break;
+                        case sf::Keyboard::Enter:
+                            Game game;
+                            switch (ventanaI.GetIndex()){
+                                case 0:
+                                    game.Run(1);
+                                    break;
+                                case 1:
+                                    game.Run(2);
+                                    break;
+                                case 2:
+                                    game.Run(3);
+                                    break;
+                                case 3:
+                                    game.Run(4);
+                                    break;
+                            }
+                            menu.close();
+                            break;
 
+                    }
+                    break;
+            }
+        }
+        menu.clear();
+        ventanaI.ventana(menu);
+        menu.display();
+    }
     return 0;
 }
+
